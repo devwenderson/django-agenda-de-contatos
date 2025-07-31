146b4e23a70986da261f60901dbc0b9ebf5c6fc1
wend.devdesigner@gmail.com <wend.devdesigner@gmail.com>
2025-07-31 16:31:47 -0300

    update: environment variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ufz(e6_pmo84n0mu$4c!2wd)2%uvd+5^71ae!)3#qffm=hvr('
+# SECRET_KEY = 'django-insecure-=ufz(e6_pmo84n0mu$4c!2wd)2%uvd+5^71ae!)3#qffm=hvr('
+
+SECRET_KEY = str(os.environ.get("DJANGO_SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
